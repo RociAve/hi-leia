17de1a12e49ed85dd608a4027abdafb6a42cea43
Rocio Avellaneda <avellanedarocioa@gmail.com>
2022-09-15 15:02:55 -0300

    Primer Commit

---
+++ src/ej1manosalaobra/Ej1ManosALaObra.java
@@ -0,0 +1,24 @@
+//Este programa equivale tmb al Ejercicio 2(Manos a la obra)de la Guia 5 de Java
+package ej1manosalaobra;
+
+public class Ej1ManosALaObra {
+
+    public static void main(String[] args) {
+        
+        int num = 5;
+        long año = 2022;
+        boolean verdadero = true;
+        double num_decimal = 10.05;
+        char inicial_caracter = 'R';
+        String cadena = "Hola, estoy aprendiendo a usar java :)";
+        
+        System.out.println(num);
+        System.out.println(año);
+        System.out.println(verdadero);
+        System.out.println(num_decimal);
+        System.out.println(inicial_caracter);
+        System.out.println(cadena);
+                
+    }
+    
+}
